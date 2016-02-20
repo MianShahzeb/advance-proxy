ea22582b792ef66f77d002ab8de5fabee567cb6f
MianShahzeb <shahzeb@yandex.com>
2016-02-20 21:02:13 +0500

    Added files via upload

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# advance-proxy
-Simple and Attractive chrome extension with bundles of poxies.
+Chrome AdvanceProxy Redirect
+=======================
+
+By: Mian Shahzeb <shahzeb@yandex.com>
+Updates By: Mian Shahzeb <https://shahzeb.xyz/>
+Github: https://github.com/MianShahzeb/advance-proxy
+
+
+
+Destription
+-----------
+
+Many universities use our Advance Proxy to allow its students access to various online
+databases.  This extension adds a button to Chrome which allows for a quick way
+to reload the current page through your Advance Proxy system.
+
+All it does is pass the URL to your library's Advance Proxy login URL.  For example:
+    http://google.com/
+would change to:
+    http://www.proxy-server-dummy-link.com/cgi-bin/r.cgi?url=http://google.com/
+
+Change Log
+----------
+
+Version 3 - January 18, 2016:
+    * Use chrome's synced storage to save the redirect url
+Version 2 - January 8, 2016:
+    * Added context menu item for links.
+    * Added drop down box in options to select school.
+Version 1 - January 3, 2016:
+    * Simple redirect button.
+	* Basics Upgrades
---
+++ background.js
@@ -0,0 +1,51 @@
+const DEFAULT_BASE_URL = "http://www.library.drexel.edu/cgi-bin/r.cgi?url=$@";
+
+function transformUrl(url, callback) {
+    chrome.storage.sync.get({"base_url": DEFAULT_BASE_URL}, function(items) {
+        var base = items["base_url"];
+        if (base.indexOf("$@") >= 0) {
+            base = base.replace("$@", url);
+        }
+        callback(base);
+    });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    transformUrl(tab.url, function(newUrl) {
+        chrome.tabs.update(tab.id, {"url": newUrl});
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    transformUrl(info.linkUrl, function(newUrl) {
+        chrome.tabs.create({"url": newUrl});
+    });
+});
+
+function initialize() {
+    chrome.contextMenus.create({
+        "title": "Open Link with Advance Proxy",
+        "contexts": ["link"],
+        "id": "redirect"
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.storage.sync.get({"base_url": null}, function(items) {
+        if (!items["base_url"]) {
+            // migrate old format
+            var legacyBase = localStorage["base_url"];
+            if (legacyBase) {
+                delete localStorage["base_url"];
+            } else {
+                legacyBase = DEFAULT_BASE_URL;
+            }
+            chrome.storage.sync.set({"base_url": legacyBase}, function() {
+                initialize();
+            });
+        } else {
+            initialize();
+        }
+    });
+});
+
